d81fe4254cd570a3a9f3d9c7c3e7e1b2d586d25d
Jason Madigan <jason.madigan@feedhenry.com>
2014-06-11 17:31:59 +0100

    Refactoring template

--- main.js
+++ main.js
@@ -4,19 +4,21 @@
  @param params.subject : email subjecrt
  @param params.body : message body
  */
-exports.sendgrid = function(params, cb){
+exports.sendgrid = function(params, cb) {
   var SendGrid = require('sendgrid').SendGrid;
   var sendgrid = new SendGrid(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
   sendgrid.send({
     to: params.to,
     from: 'mobile@feedhenry.com',
-    fromname : 'FeedHenry mBaaS',
+    fromname: 'FeedHenry mBaaS',
     subject: params.subject || "FeedHenry Email",
     text: params.body || "Here is an email from a FeedHenry app!"
   }, function(success, message) {
     if (!success) {
       return cb(message);
     }
-    return cb(null, { ok : true});
+    return cb(null, {
+      ok: true
+    });
   });
 };
